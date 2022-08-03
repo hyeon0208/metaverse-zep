8940d580d80ca55c85df4a9d340e8e005b79314d
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-08-03 20:37:59 +0900

    ADD:player title

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+// zep 앱은 main.js 압축파일만 반영됨.
 App.onJoinPlayer.Add(function(player){
     player.moveSpeed = 300; // 플레이어의 속도를 300으로 올리고
+    player.title = "SHJ";
     player.sendUpdated(); // 이 내용을 업데이트.
 })
